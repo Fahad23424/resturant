c802649f811656a17be5ab3bc41c120323d20c1b
Fahad23424 <zulfi921a@gmail.com>
2024-11-19 03:21:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const humberger =document.getElementById("humberger")
+const nav=document.getElementById("nav")
+humberger.addEventListener("click",function()
+{
+    nav.classList.toggle("show")
+})
